c55035ace0e86bc9b5e6bf99b8ee61ad64d26526
Lkhagva-erdene <anidas.com@gmail.com>
2021-09-04 10:15:29 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,39 @@
+
+public class Hello {
+
+    //variable types
+    //int ->integer buyu buhel too
+    //double butarhai too
+    //char -> character buyu temdegt
+    // ' -> single quotation mark
+    // " -> double quotation mark
+    //String -> collection of chars -> ug oguulber (temdegtuudiin tsugluulga)
+    //boolean  -> unen/hudal (true/false) -
+    //= -> assignment operator suulgaj ogoh uildel
+    public static void main(String[] args){
+        //declare -> zarlan tunhaglah
+        int x = 15;
+        double y = 15.5;
+        char ch = '$';
+        String  word = "s;qj 3m4i tpw495";
+        boolean check = true;
+
+        int a = 15;
+        int b = 20;
+        int sum = a+b;
+
+        double u = 15;
+        double u1 = 20;
+        int something = a * b + sum - a / b;
+        int something1 = a/b;
+
+ //       System.out.println("u / u1 =" + u / u1);
+
+        String oguulber = "hello";
+        oguulber = 7 + 10 + oguulber;
+        System.out.println(oguulber);
+        oguulber = oguulber + 7 * 10;
+        System.out.println(oguulber);
+
+    }
+}
